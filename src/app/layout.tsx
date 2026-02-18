28f173fabf92e87f0b3139343358032abe586b16
Weedanta <baguswedanta@student.ub.ac.id>
2026-02-18 10:11:13 +0700

    feat: Initialize Next.js project with global styles, custom Poppins fonts, and a root layout including a Navbar.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
 import "../styles/globals.css";
 import { Navbar } from "@/components/layout/navbar/components/Navbar";
-
-const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} antialiased`}>
+      <body className="antialiased">
         <Navbar />
         {children}
       </body>
