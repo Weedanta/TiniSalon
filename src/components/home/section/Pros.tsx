4d8c433c30967659bb47f374a1cb88e5e614962f
Weedanta <baguswedanta@student.ub.ac.id>
2026-02-25 05:47:03 +0700

    feat: add ProgramSection to display program offerings and Pros component for salon benefits.

--- src/components/home/section/Pros.tsx
+++ src/components/home/section/Pros.tsx
@@ -4,7 +4,7 @@ import ProsSection from "@/components/home/section/ProsSection";
 const Pros = () => {
   return (
     <section className="bg-white flex justify-center">
-      <div className="flex flex-col lg:max-w-7xl w-full px-8 md:px-8 lg:px-4 xl:px-0 xl:gap-12 md:gap-10 gap-8">
+      <div className="flex flex-col lg:max-w-7xl w-full px-8 md:px-8 lg:px-4 xl:px-0 xl:gap-12 md:gap-10 gap-4">
         <h2 className="mx-auto text-2xl md:text-4xl lg:text-5xl xl:text-6xl font-semibold text-primary-500 leading-relaxed">
           Mengapa Memilih{" "}
           <span className="mx-2 text-black-signature text-3xl md:text-5xl lg:text-6xl xl:text-7xl">
@@ -18,7 +18,7 @@ const Pros = () => {
           pelanggan.
         </p>
 
-        <div className="w-full mt-8 md:mt-12">
+        <div className="w-full mt-6 md:mt-12">
           <ProsSection />
         </div>
       </div>
